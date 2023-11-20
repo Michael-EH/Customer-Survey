48e76a29ba2937c653ab2e4b636ddbcc3bc945c4
Michael.EH <michael.ehinze@sunrisesoftware.com>
2023-11-20 17:35:10 +0000

    made some progress on responsiveness today, despite lots of trial and error. Thankfully, was savvy enough to do this in a new branch dedicated to this in case something goes wrong

--- survey/src/Components/InitialScreen.js
+++ survey/src/Components/InitialScreen.js
@@ -45,17 +45,17 @@ function InitialScreen() {
         {/*The container is an MuI component that contains the rest of the code inside and applies a certain width and height to the page, akin to a div element*/}
       <img className='logo' src={img6} alt=''/>
         <Container>
-        <Box sx={{display: 'flex'}}>
+        <Box className='specbox' sx={{display: 'flex'}}>
         {/*The Typography element is the header and paragraph wrapped in one, this particular line is for the title of the survey on the page */}
-        {showContainer && <Typography variant='h3' sx={{my:6, mx: 6}}><SpecialText /></Typography>}
+        {showContainer && <Typography xs={12} sm={6} md={2} variant='h3' sx={{my:6, mx: 6}}><SpecialText /></Typography>}
         </Box>
         {showContainer && <hr style={{color:'black', border: '1px solid black'}}/>}
         {/*This is the short paragraph that uses lorem ipsum. Just do ctrl+shift+p to open the command pallete and type in ">lorem ipsum" */}
-        {showContainer && <Typography xs={12} sm={6} md={2} paragraph sx={{my:4, mx: 2}}>Ipsum non ex proident elit deserunt laboris sit minim ad eu nostrud officia id voluptate. Et ex ipsum enim consequat. Incididunt ea culpa et tempor officia occaecat et. Dolor nisi irure ullamco id. Nostrud cupidatat tempor mollit irure incididunt qui excepteur incididunt. Irure sit labore consequat labore Lorem velit. Et et nostrud minim minim excepteur tempor.
+        {showContainer && <h3 className='ipsum' xs={12} sm={6} md={2} paragraph sx={{my:4, mx: 2}}>Ipsum non ex proident elit deserunt laboris sit minim ad eu nostrud officia id voluptate. Et ex ipsum enim consequat. Incididunt ea culpa et tempor officia occaecat et. Dolor nisi irure ullamco id. Nostrud cupidatat tempor mollit irure incididunt qui excepteur incididunt. Irure sit labore consequat labore Lorem velit. Et et nostrud minim minim excepteur tempor.
 
 Voluptate esse reprehenderit proident reprehenderit Lorem consectetur. Sint amet ullamco proident esse non. Excepteur eiusmod minim duis pariatur nulla cupidatat culpa aliqua enim anim do. Ad sit adipisicing adipisicing cillum.
 
-Occaecat consectetur id elit cupidatat occaecat laborum in do enim. Cupidatat quis fugiat sit aute pariatur enim incididunt nisi ex. Aliqua reprehenderit incididunt aliqua voluptate in ullamco aliquip nulla tempor.</Typography>}
+Occaecat consectetur id elit cupidatat occaecat laborum in do enim. Cupidatat quis fugiat sit aute pariatur enim incididunt nisi ex. Aliqua reprehenderit incididunt aliqua voluptate in ullamco aliquip nulla tempor.</h3>}
 {/* The Box component is a generic, theme-aware container with access to CSS utilities from MUI System. Its currently wrapping the Paper and Grid elements */}
         <Box
       sx={{
@@ -68,8 +68,8 @@ Occaecat consectetur id elit cupidatat occaecat laborum in do enim. Cupidatat qu
       }}
     >
 {/* The Paper element creates square like designs. These will be used for the box that acts as the background for the faces and submit button */}
-      <Paper elevation={3} style={{backgroundColor: 'rgba(255, 255, 255, 0.3)', backdropFilter: 'blur(3px)', padding: '30px 40px', margin: '20px 95px'}}>
-      {showContainer && <Grid container sx={{m:3, gap:4, pb: 3}}>
+      <Paper className='cardcontainer' elevation={3} style={{backgroundColor: 'rgba(255, 255, 255, 0.3)', backdropFilter: 'blur(3px)', padding: '30px 40px', margin: '20px 95px'}}>
+      {showContainer && <Grid container sx={{m:3, gap:4, pb: 3}} className='grid-container'>
 {/* The Grid element from MuI is also like the regular Grid element which ceates columns to organise the placement of the faces */}
 {/* Re-implemented Paper element and figured out how to connect muI with framer motion for the emotive faces the user will pick */}
 {/* Implemented Framer Motion for hover effects */}
@@ -138,9 +138,17 @@ Occaecat consectetur id elit cupidatat occaecat laborum in do enim. Cupidatat qu
         {showComment && showContainer && <TextField sx={{mt: -2}} id="outlined-basic" label="Write your review here" variant="outlined" fullWidth />}
         {showError404 && <ErrorScreen/>}
         {showError202 && <SecondErrorScreen/>}
-        {showComment && showContainer && <Button variant="outlined" sx={{mt: 2}} onClick={() => {throttle(); error202();}}>error 202</Button>}
-        {showComment && showContainer && <Button variant="outlined" sx={{mt: 2, mx: 2}} onClick={() => {throttle(); congratulate();}}>SUBMIT</Button>}
-        {showComment && showContainer && <Button variant="outlined" sx={{mt: 2}} onClick={() => {throttle(); error();}}>error 404</Button>}
+        <div className='button-container'>
+        <div className='button-div bd1'>
+        {showComment && showContainer && <Button className='button-design' variant="outlined" sx={{mt: 2}} onClick={() => {throttle(); error202();}}>error 202</Button>}
+        </div>
+        <div className='button-div bd2'>
+        {showComment && showContainer && <Button className='button-design' variant="outlined" sx={{mt: 2, mx: 2}} onClick={() => {throttle(); congratulate();}}>SUBMIT</Button>}
+        </div>
+        <div className='button-div bd3'>
+        {showComment && showContainer && <Button className='button-design' variant="outlined" sx={{mt: 2}} onClick={() => {throttle(); error();}}>error 404</Button>}
+        </div>
+        </div>
       {showThanksMessage && <ThankScreen />}
       </Paper>
     </Box>
--- survey/src/Components/SpecialText.js
+++ survey/src/Components/SpecialText.js
@@ -4,15 +4,10 @@ import React from 'react'
 import TextSpan from '../Components/TextSpan';
 
 function SpecialText() {
-    const sentence = 'Welcome To Our Sunrise Survey'.split('');
   return (
-    <div className='SpecText' style={{marginTop: '1rem', fontSize: '3rem', display: 'inline-block'}}>{sentence.map((letter, index) =>{
-        return(
-            <TextSpan key={index}>
-                {letter === ' ' ? '\u00A0' : letter}
-            </TextSpan>
-        )
-    })}</div>
+    <div className='SpecText'>
+      <h1>Welcome To Our Sunrise Survey</h1>
+    </div>
   )
 }
 
